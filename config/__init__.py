0cc43ceddb7fd4a948941e5acc3f23fa108da81e
vinodhconnects <vinodhconnects@gmail.com>
2023-07-19 12:24:20 +0530

    peopletable

--- config/__init__.py
+++ config/__init__.py
@@ -6,8 +6,7 @@ app=Flask(__name__) # Flask object that manages entire application
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
