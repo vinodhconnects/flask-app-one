a2b6c65cc76ee25fbd07aba22c3f84fa29d85c5a
vinodhconnects <vinodhconnects@gmail.com>
2023-07-19 11:31:04 +0530

    filelines

--- config/__init__.py
+++ config/__init__.py
@@ -3,4 +3,4 @@ from flask import Flask
 app=Flask(__name__) # Flask object that manages entire application
 
 def getDataFile():
-    return open("staticdata.txt","r+a")
+    return open("staticdata.txt","r")
