9385d8a2a76a7e7d2e41e4296fb2ebf5e504bea2
vinodhconnects <vinodhconnects@gmail.com>
2023-07-20 09:09:43 +0530

    peoplechange

--- dbmodels/people.py
+++ dbmodels/people.py
@@ -5,7 +5,10 @@ class People(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30),nullable=False)
     city=db.Column(db.String(30),nullable=False)
-    
+    def __init__(self,sno,name,city):
+        self.sno=sno
+        self.name=name
+        self.city=city
     def serialize(self):
          return {
                 'sno': self.sno,
