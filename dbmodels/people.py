c5d7d0cd68f8aa40c99428f4614be0e2eb9f18dd
vinodhconnects <vinodhconnects@gmail.com>
2023-07-20 08:52:41 +0530

    modification

--- dbmodels/people.py
+++ dbmodels/people.py
@@ -1,15 +1,16 @@
 from config import db
 
 class People(db.Model):
+    #__tablename__='people'
     sno=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30),nullable=False)
     city=db.Column(db.String(30),nullable=False)
-   
-    def __repr__(self):
-        return str(
-            {
+    
+    def serialize(self):
+         return {
                 'sno': self.sno,
                 'name': self.name,
                 'city': self.city
             }
-        )
+    def __repr__(self):
+        return str(self.serialize())
